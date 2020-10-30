a90a15ce63c0b6f74dd446e5b6f849a855ed6260
Vidar Eldøy <vidar@eldoy.com>
2020-10-30 05:01:09 +0100

    Clean up server

--- server.js
+++ server.js
@@ -9,11 +9,9 @@ const CONFIGS = {
 }
 
 const server = http.createServer((req, res) => {
-  // const conf = CONFIGS[]
   const query = req.url.split('?')[1]
   const config = query ? query.split('conf=')[1] : '0'
   const conf = CONFIGS[config] || { dir: 'dist' }
-  console.log({ conf })
   hangersteak(req, res, conf)
 })
 
