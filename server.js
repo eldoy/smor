b96bc3aa7dd1974aaef72b1c6c9ffbaa9895bec3
Vidar Eldøy <vidar@eldoy.com>
2021-03-09 18:34:28 +0100

    Remove a log

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const server = http.createServer((req, res) => {
   const query = req.url.split('?')[1]
   const config = query ? query.split('conf=')[1] : '0'
   const conf = CONFIGS[config] || { dir: 'dist' }
-  console.log({ conf })
   hangersteak(req, res, conf)
 })
 
