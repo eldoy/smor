bd1018d1339f2fab13369deba7f14a0aa533c0ed
Vidar Eldøy <vidar@eldoy.com>
2026-03-19 00:11:13 +0100

    fix: remove range support

--- index.js
+++ index.js
@@ -17,24 +17,20 @@ var DEFAULT_OPTIONS = {
   compress: false
 }
 
-// Sends the request
 function send(res, status) {
   res.statusCode = status
   res.end()
 }
 
-// Check if we already encoded compression
 function encoded(res) {
   return (res.getHeader('content-encoding') || 'identity') !== 'identity'
 }
 
-// Don't transform if cache-control no-transform is set
 function transformable(res) {
   var cacheControl = res.getHeader('cache-control')
   return !cacheControl || !NOTRANSFORM.test(cacheControl)
 }
 
-// Find accepted compressor algorithm
 function accepted(req) {
   var accept = accepts(req)
   return (
@@ -44,19 +40,19 @@ function accepted(req) {
   )
 }
 
-// Apply correct compressor, default is gzip
 function compressor(algorithm) {
   if (algorithm === 'deflate') return zlib.createDeflate()
   if (algorithm === 'br') return zlib.createBrotliCompress()
   return zlib.createGzip()
 }
 
-// Set up a read stream
 function pipe(req, res, options, fileName, filePath) {
-  return function (status, headers, length, start, end) {
+  return function (status, headers, length) {
     var type = mime.lookup(fileName) || 'application/octet-stream'
     res.setHeader('content-type', mime.contentType(type))
-    var stream = fs.createReadStream(filePath, { start, end })
+
+    var stream = fs.createReadStream(filePath)
+
     if (
       options.compress &&
       req.method !== 'HEAD' &&
@@ -73,12 +69,12 @@ function pipe(req, res, options, fileName, filePath) {
     } else {
       res.setHeader('content-length', length)
       res.writeHead(status, headers)
+      if (req.method === 'HEAD') return res.end()
       stream.pipe(res)
     }
   }
 }
 
-// Get file stats
 async function fileStats(filePath) {
   try {
     return await new Promise(function (resolve, reject) {
@@ -89,7 +85,6 @@ async function fileStats(filePath) {
   } catch (e) {}
 }
 
-// Set up file asset
 async function asset(req, filePath) {
   var stat = await fileStats(filePath)
   if (!stat) return null
@@ -97,56 +92,36 @@ async function asset(req, filePath) {
   var modifiedDate = new Date(Date.parse(modifiedSince))
   var lastModified = new Date(stat.mtime)
   var fresh = modifiedSince && modifiedDate >= lastModified
-  return { stat, modifiedSince, modifiedDate, lastModified, fresh }
+  return { stat, lastModified, fresh }
 }
 
-// Main function
 module.exports = async function (req, res, customOptions = {}) {
   var options = { ...DEFAULT_OPTIONS, ...customOptions }
 
-  // Parse request if pathname is missing
   if (!req.pathname) rekvest(req)
 
-  // File name and path
   let fileName = req.pathname
   if (fileName.endsWith('/')) fileName += options.indexFile
 
   var base = options.dir.startsWith('/')
     ? options.dir
     : path.join(ROOT, options.dir)
+
   var filePath = path.join(base, fileName)
 
-  // Look for requested file
   var file = filePath.startsWith(base) ? await asset(req, filePath) : null
 
-  // Return 404 if not found
   if (!file) return send(res, 404)
 
-  // Return 304 Not Modified if possible
   if (file.fresh) return send(res, 304)
 
-  // Stream file if it exists
   var stream = pipe(req, res, options, fileName, filePath)
   var totalSize = file.stat.size
-  var range = req.headers.range
 
-  // Stream the full file if no range requested
-  if (!range) {
-    var headers = {
-      'cache-control': `max-age=${options.maxAge}`,
-      'last-modified': file.lastModified.toUTCString()
-    }
-    return stream(200, headers, totalSize)
-  }
-
-  // Return a byte range if the client asks for it
-  var parts = range.replace(/bytes=/, '').split('-')
-  var start = parseInt(parts[0])
-  var end = parts[1] ? parseInt(parts[1]) : totalSize - 1
-  var chunkLength = end - start + 1
   var headers = {
-    'content-range': `bytes ${start}-${end}/${totalSize}`,
-    'accept-ranges': 'bytes'
+    'cache-control': `max-age=${options.maxAge}`,
+    'last-modified': file.lastModified.toUTCString()
   }
-  stream(206, headers, chunkLength, start, end)
+
+  return stream(200, headers, totalSize)
 }
